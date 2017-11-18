7b6dad4ea9ef804fda93de80eea515c1459b6cad
sahasakkhar <sakkhar@mail.com>
2017-11-18 16:21:04 +0600

    Food Delivery App Using Django

--- foodtasker/models.py
+++ foodtasker/models.py
@@ -12,8 +12,14 @@ class Restaurant(models.Model):
     def __str__(self):
         return self.name
 
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='customer')
+    avatar = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+    address= models.TextField()
 
-
+    def __str__(self):
+        return self.user.get_full_name()
 
 
 
