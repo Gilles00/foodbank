a7c582e75f19d907fd495c0261e4f2db7857c872
sahasakkhar <sakkhar@mail.com>
2017-11-18 16:17:52 +0600

    Initial commit

---
+++ foodtasker/models.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+class Restaurant(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE, related_name='restaurant')
+    name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+    address = models.TimeField()
+    logo = models.ImageField(upload_to='restaurant_logo/', blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
